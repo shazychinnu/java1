076e0b6eb5c8ec0be53957cf5daad4086cc0b39e
aravind <aravindersuram@gmail.com>
2020-04-29 03:05:39 +0530

    add part time

--- empwage/Empusercase.java
+++ empwage/Empusercase.java
@@ -18,8 +18,12 @@ public class Empusercase{
 
 		//Print if employee is present or absent
 		if(presence==1){
-                        double wage=utils.wageCalculation();
-			System.out.println("Employee wage is:"+wage);
+                        if(utils.isFullTime()==1){
+				System.out.println("Employee wage for full time is "+ utils.FtWageCalculation());
+			}
+			else{
+				System.out.println("Employee wage for part time is "+ utils.PtWageCalculation());
+			}
 		}
 		else{
 			System.out.println("Employee is absent");
@@ -46,17 +50,45 @@ public class Empusercase{
                         return(0);
                 }
         }
+          /**
+	*Check if the employee works parttime or fulltime
+	*/
+
+	public int isFullTime(){
+
+		int IS_FULLTIME=1;
+		double isFulltime=Math.floor(Math.random()*10)%2;
+		if(isFulltime==IS_FULLTIME){
+			return(1);
+		}
+		else{
+			return(0);
+		}
+	}
 
         /**
         *calculate the employee wage
         */
 
-	 public double wageCalculation(){
+	 public double FtWageCalculation(){
 		double wagePerHour=20;
 		double fullDay=8;
 	        double empWage;
                 empWage=wagePerHour*fullDay;
 		return(empWage);
 	}
+
+
+/**
+        *calculate the parttime employee wage
+        */	
+	public double PtWageCalculation(){	
+		double wagePerHour=20;
+                double fullDay=4;
+                double empWage;
+
+                empWage=wagePerHour*fullDay;
+                return(empWage);
+        }
 
 }
